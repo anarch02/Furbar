5b928be5326b72f02604d98e4bc3e45d7a6350c8
anarch02 <mrjianarchist@gmail.com>
2024-06-30 15:04:23 +0500

    install filament, chained-translation-manager, laravel-sluggable

--- config/app.php
+++ config/app.php
@@ -79,6 +79,10 @@ return [
     */
 
     'locale' => env('APP_LOCALE', 'en'),
+    'locales' => [
+        'en' => 'English',
+        'ru' => 'Russian',
+    ],
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
